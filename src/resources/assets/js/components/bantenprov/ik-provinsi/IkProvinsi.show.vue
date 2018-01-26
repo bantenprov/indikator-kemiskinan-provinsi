681bbb41a518981bcad1d32c2c6c2557f7886a1c
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2018-01-26 13:35:45 +0700

    renamed folder

---
+++ src/resources/assets/js/components/bantenprov/ik-provinsi/IkProvinsi.show.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <div class="card mb-3">
+      <div class="card-header">Grafik Produk Domestik Regional Bruto</div>
+      <div class="card-body">
+        <echarts-ik-provinsi></echarts-ik-provinsi>
+      </div><!-- /.card-body -->
+    </div><!-- /.card -->
+
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card mb-3">
+          <div class="card-header">Grafik Kota</div>
+          <div class="card-body">
+            <echarts-ik-provinsi-kota></echarts-ik-provinsi-kota>
+          </div><!-- /.card-body -->
+        </div><!-- /.card -->
+      </div>
+      <div class="col-md-6">
+        <div class="card mb-3">
+          <div class="card-header">Grafik Tahunan</div>
+          <div class="card-body">
+            <echarts-ik-provinsi-tahun></echarts-ik-provinsi-tahun>
+          </div><!-- /.card-body -->
+        </div><!-- /.card -->
+      </div>
+    </div>
+
+  </div>
+</template>
